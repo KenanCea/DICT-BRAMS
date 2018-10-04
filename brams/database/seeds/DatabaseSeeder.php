fc5521a548e24e332d60039b012b5d6c95231c94
janr22 <32945248+janr22@users.noreply.github.com>
2018-10-04 19:04:34 +0800

    seed db

--- brams/database/seeds/DatabaseSeeder.php
+++ brams/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //        $this->call(UsersTableSeeder::class);
-        factory(App\User::class,10)->create();
-        
+        if (DB::table('users')->get()->count() == 0) {
+            DB::table('users')->insert([
+                [
+                    'name' => ('admin'),
+                    'email' => ('admin@gmail.com'),
+                    'password' => bcrypt('secret'),
+                    'type' => ('admin'),
+                    'photo' => ('logo.png'),
+                    'remember_token' => str_random(10),
+                    'email_verified_at' => now(),
+                ], [
+                    'name' => ('user'),
+                    'email' => ('user@gmail.com'),
+                    'password' => bcrypt('secret'),
+                    'type' => ('user'),
+                    'photo' => ('logo.png'),
+                    'remember_token' => str_random(10),
+                    'email_verified_at' => now(),
+                ],
+            ]);
+        } else {echo "\e[31mTable is not empty, therefore NOT ";}
+
     }
 }
