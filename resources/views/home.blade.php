a86ee68cb4a024bfcf2dce5218bd59f15a621c41
janr22 <32945248+janr22@users.noreply.github.com>
2019-03-28 08:14:23 +0800

    renew

--- resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<vue-layout></vue-layout>
-@endsection
--- storage/framework/views/784ddadbc5380b7cd1e5f98f543b8efa980c8d00.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<vue-layout></vue-layout>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
