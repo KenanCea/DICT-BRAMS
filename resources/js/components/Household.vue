f4bc2f47fb30f964562279d3874d6db8343de2c3
Josh <2165833@slu.edu.ph>
2019-06-03 03:21:29 +0800

    archive

--- resources/js/components/Household.vue
+++ resources/js/components/Household.vue
@@ -1242,18 +1242,7 @@ export default {
         })
         .catch(() => {});
     },
-    updateInhabitant() {
-      this.inhabitantForm
-        .put("api/inhabitant/" + this.inhabitantForm.id)
-        .then(() => {
-          this.dialogCreateInhabitant = false;
-          toast.fire({
-            type: "success",
-            title: "Inhabitants has been edited"
-          });
-        })
-        .catch(() => {});
-    },
+    
 
     updateHousehold() {
       this.householdForm
