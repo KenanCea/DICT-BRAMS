9eec1dbc3107ea11aa07bdd404be1c3f2429fe82
Idris Gabriel Mabunga Galiste <32945358+2160062@users.noreply.github.com>
2019-06-04 22:23:05 +0800

    Updated BarangayInformation.vue
    Now included save database fuction

--- resources/js/components/BarangayInformation.vue
+++ resources/js/components/BarangayInformation.vue
@@ -39,54 +39,68 @@
               <v-layout>
                 <v-flex md12>
                   <v-data-table
-                    :headers="headers"
+                    id="barangayOfficials"
+                    :headers="headersOfficials"
                     :items="officials"
                     hide-default-footer>
                     <template v-slot:top>
-                      <v-dialog v-model="dialog" max-width="300px">
-                        <v-card>
-                          <v-card-title>
-                            <span class="headline">Edit Row</span>
-                          </v-card-title>
-    
-                          <v-card-text>
-                            <v-container grid-list-md>
-                              <v-layout wrap>
-                                <v-flex xs12 md12>
-                                  <v-text-field v-model="editedItem.name" label="Barangay Official name"></v-text-field>
-                                </v-flex>
-                                <v-flex xs12 md6>
-                                  <v-text-field v-model="editedItem.start" label="Start Term"></v-text-field>
-                                </v-flex>
-                                <v-flex xs12 md6>
-                                  <v-text-field v-model="editedItem.end" label="End Term"></v-text-field>
-                                </v-flex>
-                              </v-layout>
-                            </v-container>
-                          </v-card-text>
-    
-                          <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                            <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                          </v-card-actions>
-                        </v-card>
+                      <v-dialog v-model="dialogOfficial" max-width="300px">
+                        <v-form
+                          ref="formOfficials"
+                          v-model="valid"
+                          lazy-validation 
+                          @submit.prevent="updateOfficial()">
+                          <v-card>
+                            <v-card-title>
+                              <span class="headline">Edit Row</span>
+                            </v-card-title>
+      
+                            <v-card-text>
+                              <v-container grid-list-md>
+                                <v-layout wrap>
+                                  <v-flex xs12 md12>
+                                    <v-text-field v-model="officialForm.name" label="Barangay Official name"></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 md6>
+                                    <v-text-field v-model="officialForm.start_term" label="Start Term"></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 md6>
+                                    <v-text-field v-model="officialForm.end_term" label="End Term"></v-text-field>
+                                  </v-flex>
+                                </v-layout>
+                              </v-container>
+                            </v-card-text>
+      
+                            <v-card-actions>
+                              <v-spacer></v-spacer>
+                              <v-btn color="blue darken-1" text @click="dialogOfficial = false">Cancel</v-btn>
+                              <v-btn 
+                                color="blue darken-1" 
+                                text type="submit" 
+                                @click="submitOfficials"
+                              >Save</v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-form>
                       </v-dialog>
+                    </template>
+
+                    <template v-slot:items="props">
+                      <td>{{ props.item.position }}</td>
+                      <td>{{ props.item.name }}</td>
+                      <td>{{ props.item.start_term }}</td>
+                      <td>{{ props.item.end_term }}</td>
                     </template>
 
                     <template v-slot:item.action="{ item }">
                       <v-tooltip bottom>
                         <template v-slot:activator="{ on }">
-                          <v-btn text icon @click="editItem(item)" v-on="on">
+                          <v-btn text icon @click="editOfficialDialog(item)">
                             <v-icon>mdi-pencil</v-icon>
                           </v-btn>
                         </template>
                         <span>Edit</span>
                       </v-tooltip>
-                    </template>
-
-                    <template #no-data>
-                      <v-btn color="primary" @click="initialize">Reset</v-btn>
                     </template>
                   </v-data-table>
                 </v-flex>
@@ -101,150 +115,60 @@
 <script>
   export default {
     data: () => ({
-      dialog: false,
-      headers: [
-        {
-          text: 'POSITION',
-          value: 'position',
-          align: 'center',
-          sortable: false
-        },
-        { 
-          text: 'NAME',
-          value: 'name',
-          align: 'center',
-          sortable: false    
-        },
-        { 
-          text: 'START TERM', 
-          value: 'start', 
-          align: 'center',
-          sortable: false
-        },
-        { 
-          text: 'END TERM', 
-          value: 'end', 
-          align: 'center',
-          sortable: false
-        },
-        { 
-          text: 'Actions', 
-          value: 'action',
-          align: 'center', 
-          sortable: false 
-        }
+      Table: "Officials",
+      Orientation: "landscape",
+      dialogOfficial: false,
+      headersOfficials: [
+        { text: 'Position', value: 'position', align: 'center', sortable: false },
+        { text: 'Name', value: 'name', align: 'center', sortable: false },
+        { text: 'Start Term', value: 'start_term', align: 'center', sortable: false },
+        { text: 'End Term', value: 'end_term', align: 'center', sortable: false },
+        { text: 'Actions', value: 'action', align: 'center', sortable: false }
       ],
+      valid: true,
       officials: [],
-      editedIndex: -1,
-      editedItem: {
-        name: '',
-        start: 0,
-        end: 0
-      },
-      defaultItem: {
-        name: '',
-        start: 0,
-        end: 0
-      }
+      officialForm: new Form({
+        id: "",
+        position: "",
+        name: "",
+        start_term: "",
+        end_term: ""
+      })
     }),
 
-    watch: {
-      dialog (val) {
-        val || this.close()
-      }
-    },
-
     created () {
-      this.initialize()
+      this.getOfficials()
     },
 
     methods: {
-      initialize () {
-        this.officials = [
-          {
-            position: 'Punong Barangay',
-            name: 'Gati, Randy Tadique',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Anton, Brian Anton Cornel',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Willy, Arthur Tony',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Contic, Martin Galino',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Balleras, Emily Ramos',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Amistad, Rudy Date',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Zapanta, Vincente Jubilo',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Kagawad',
-            name: 'Columbres, Modesto Pagaway',
-            start: '2013',
-            end: '2016'
-          },
-          {
-            position: 'Barangay Treasurer',
-            name: '',
-            start: '2013',
-            end: '2016' 
-          },
-          {
-            position: 'Barangay Secretary',
-            name: '',
-            start: '2013',
-            end: '2016'
-          }
-        ]
+      getOfficials() {
+        axios.get("api/official").then(response => {
+          this.officials = response.data;
+        });
       },
 
-      editItem (item) {
-        this.editedIndex = this.officials.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
+      updateOfficial() {
+        this.officialForm
+          .put("api/official/" + this.officialForm.id)
+          .then(() => {
+            this.dialogOfficial = false;
+            this.getOfficials();
+            toast.fire({
+              type: "success",
+              title: "Official has been edited"
+            });
+          })
+          .catch(() => {});
       },
 
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
+      editOfficialDialog(officials) {
+        this.officialForm.reset();
+        this.dialogOfficial = true;
+        this.officialForm.fill(officials);
       },
 
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.officials[this.editedIndex], this.editedItem)
-        } else {
-          this.officials.push(this.editedItem)
-        }
-        this.close()
+      submitOfficials() {
+        this.$refs.formOfficials.validate();
       },
 
       updateLogo(e) {
