465243f720a058348c3a2e579c56d18b5534827c
Idris Gabriel Mabunga Galiste <32945358+2160062@users.noreply.github.com>
2019-06-25 01:01:00 +0800

    Update Education.php

--- app/Education.php
+++ app/Education.php
@@ -7,15 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Education extends Model
 {
     protected $fillable = [
-        'education_type',
-        'education_number',
-        'sports_type',
-        'sports_number',
+        'no_pre_school_building',
+        'no_primary_building',
+        'no_secondary_building',
+        'no_vocational_building',
+        'no_college_building',
+        'no_post_graduate_building',
+        'no_covered_courts',
+        'no_gymnasiums',
+        'no_parks_plaza',
+        'others_sport_facilities',
+        'recreational_activities',
         'total_facilities'
     ];
+    
     public function barangay()
     {
         return $this->belongsTo('App\Barangay');
     }
 }
-
