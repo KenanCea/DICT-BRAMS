3fc004457b4ed712938d9f10782a0ed26f9d9676
Idris Gabriel Mabunga Galiste <32945358+2160062@users.noreply.github.com>
2019-06-25 01:04:33 +0800

    Update BarangayController.php

--- app/Http/Controllers/BarangayController.php
+++ app/Http/Controllers/BarangayController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Barangay;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class BarangayController extends Controller
 {
@@ -15,16 +16,53 @@ class BarangayController extends Controller
 
     public function index()
     {
-        return Barangay::leftJoin('users', 'barangays.user_id', '=', 'users.id')
+        return DB::table('barangays')
+            ->leftJoin('users', 'barangays.user_id', '=', 'users.id')
             ->select('barangays.*')
             ->where('users.id', Auth::user()->id)
             ->get();
     }
 
-    public function update(Request $request, $id)
+    public function LandForm()
     {
-        $barangay = Barangay::findOrFail($id);
+        //Mountainous
+        $query = Barangay::leftJoin('users', 'barangays.user_id', '=', 'users.id')
+        ->selectRaw('"Mountainous" as "landform_type", mountainous as "landform_percent"', [Auth::user()->id]
+        )
+        ->where('users.id',Auth::user()->id);
+        //Plain
+        $querytwo = Barangay::leftJoin('users', 'barangays.user_id', '=', 'users.id')
+        ->selectRaw('"Plain" as "landform_type", plain as "landform_percent"', [Auth::user()->id]
+        )
+        ->where('users.id',Auth::user()->id);
+        //Valley
+        $querythree = Barangay::leftJoin('users', 'barangays.user_id', '=', 'users.id')
+        ->selectRaw('"Valley" as "landform_type", valley as "landform_percent"', [Auth::user()->id]
+        )
+        ->where('users.id',Auth::user()->id);
+        $query->union($querytwo)->union($querythree);
+
+        $query=$query->get();
+        return $query;
+    }
+
+    public function updateBarangay(Request $request)
+    {
+        $user = auth('api')->user();
         $barangay->update($request->all());
+        return ['message' => "Success"];
+    }
+
+    public function updateLandForm(Request $request, $id)
+    {
+        $landForm = Barangay::findOrFail($id);
+        $landForm->update($request->all());
+
+        $name=User::findOrFail(Auth::user()->id);
+        $logs= new activitylogs;
+        $logs->log="Updated percentage of  Land Forms ".$landForm->mountainous.", ".$landForm->plain.",  ".$landForm->valley.", ".$name->name;
+        $logs->user_id=Auth::user()->id;
+        $logs->save();
     }
 
     public function barangay()
