a68dc7c226fc2b0e0fde4aece908fee27f4b00ca
Idris Gabriel Mabunga Galiste <32945358+2160062@users.noreply.github.com>
2019-06-25 01:00:24 +0800

    Update HealthFacilities.php

--- app/HealthFacilities.php
+++ app/HealthFacilities.php
@@ -7,11 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class HealthFacilities extends Model
 {
     protected $fillable = [
-        'healthfacility_type',
-        'number',
-        'numpatients',
+        'pubhos_num',
+        'pubhos_numpatients',
+        'prihos_num',
+        'prihos_numpatients',
+        'healthcenter_num',
+        'healthcenter_numpatients',
+        'clinic_num',
+        'clinic_numpatients',
+        'self_medication_num',
+        'self_medication_numpatients',
         'otherSpecify',
     ];
+    
     public function barangay()
     {
         return $this->belongsTo('App\Barangay');
