0d97ac5f5252496676a79655b6471056f8c78b6d
Idris Gabriel Mabunga Galiste <32945358+2160062@users.noreply.github.com>
2019-06-25 01:01:56 +0800

    Update Communication.php

--- app/Communication.php
+++ app/Communication.php
@@ -7,14 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Communication extends Model
 {
     protected $fillable = [
-        'communication_type', 
-        'totalProvider',
-        'name_company', 
-        'no_hhold_served'
+        'telephone_totalProvider',
+        'telephone_name_company',
+        'telephone_no_hhold_served',
+        'cellular_totalProvider',
+        'cellular_name_company',
+        'cellular_no_hhold_served',
+        'internet_totalProvider',
+        'internet_name_company',
+        'internet_no_hhold_served',
+        'postalservive_totalProvider',
+        'postalservive_name_company',
+        'postalservive_no_hhold_served',
+        'deliveryservice_totalProvider',
+        'deliveryservice_name_company',
+        'deliveryservice_no_hhold_served'
     ];
+    
     public function barangay()
     {
         return $this->belongsTo('App\Barangay');
     }
 }
-
